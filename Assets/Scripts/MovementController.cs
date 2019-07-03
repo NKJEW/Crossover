2a7476964025ebd16e5b63bcad94ee61d2e28830
Nathaniel Jew <nathankjew@gmail.com>
2019-07-02 20:34:57 -0700

    New folders

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+    Rigidbody2D rb;
+
+    void Start() {
+        rb.centerOfMass = new Vector2(0, -0.5f);
+    }
+
+}
